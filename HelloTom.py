4a1de5b5d69fd27afed24bfc61676ecec143fffa
AnnihilatorGamer <30694343+AnnihilatorGamer@users.noreply.github.com>
2022-06-09 18:58:18 +0100

    Add files via upload

---
+++ HelloTom.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+
+print ("Hello " + name)
+
+secret = input("would you like to know a secret? ")
+
+if secret == "yes":
+   print ("Can I sleep or have a smoke")
+
+else:
+     print ("Well fuck you then")
+
