3eeb9bae582df94bc25e723e196fe194abf007da
AnnihilatorGamer <30694343+AnnihilatorGamer@users.noreply.github.com>
2022-06-10 09:37:54 +0100

    Update HelloTom.py

--- HelloTom.py
+++ HelloTom.py
@@ -5,7 +5,7 @@ print ("Hello " + name)
 secret = input("would you like to know a secret? ")
 
 if secret == "yes":
-   print ("Can I sleep or have a smoke")
+   print ("Can I sleep tonight? ")
 
 else:
      print ("Well fuck you then")
